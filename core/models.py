6d0add9ec6ee2bf61f40c1f90a5a4be36b95b37a
jean <jeanb.santos404@gmail.com>
2025-04-19 16:44:22 -0300

    Chore: inicialição do projeto

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+git init
+git add .
+git commit -m "Chore: inicialição do projeto "
+git branch -M main
+git remote add origin https://github.com/jeanbar1/cardapio.git
+git push -u origin main
