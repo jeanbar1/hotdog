ca650ee0ecbd68f5909d6caf7af013db2b5799cb
jean <jeanb.santos404@gmail.com>
2025-05-06 15:39:46 -0300

    correção

--- cardapio/settings.py
+++ cardapio/settings.py
@@ -53,6 +53,13 @@ AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
 
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+
 # ==================== CONFIGURAÇÕES DE ARMAZENAMENTO ====================
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
